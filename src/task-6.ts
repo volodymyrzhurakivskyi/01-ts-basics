1405d0dd8506d73629565b584ca20109888de2cb
volodymyr <zhurakivskyivolodymyr@gmail.com>
2026-06-06 15:16:27 +0300

    task_1-8

---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([true, false, true]));
